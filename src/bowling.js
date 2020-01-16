e2ce0ee706155c9b808f7a383af3b898801b4326
Naïma Mansour <naimamansour@MacBook-Pro-de-Naima.local>
2020-01-16 17:28:40 +0100

    bowling tdd - test one

---
+++ src/bowling.js
@@ -0,0 +1,28 @@
+
+class BowlingGame {
+    constructor(players, rolls, img, pins) {
+    this.players = 0;
+    this.rolls = [];
+    this.img = 10;
+    this.pins = 0;
+
+    }
+
+    /*isplayers(){
+        this.player++;
+        if (this.player <=0)
+        this.player = false;
+        resturn 'we shall have at least one player'
+    }*/
+
+
+    /*roll(pins) {
+      this.rolls.push(pins);
+    }
+    score() {
+      return this.rolls.reduce((roll, total) => total + roll);
+    }*/
+
+  }
+  
+  module.exports = BowlingGame;
---
+++ test/bowling.spec.js
@@ -0,0 +1,17 @@
+const assert = require('chai').assert;
+
+const BowlingGame = require('../src/bowling');
+
+describe('BowlingGame', function() {
+
+    it('should be a function', function(){
+        assert.isFunctions(BowlingGame);
+    });
+     
+    it('should return one player', function() {
+        let game = new BowlingGame()
+        assert.isAtLeast();
+      
+    });
+  });
+
